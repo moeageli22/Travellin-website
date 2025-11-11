b6064102e8a05f18d924ac4e377db7bd584e0849
Kingofthemducks <s4300319@lsbu.ac.uk>
2025-11-11 11:55:55 +0000

    So, made changes to the signup page in app and linked it to users table in supabase and the supabaseClient.ts in lib and specified the URL and publishable key in that file instead of an external one which I have tested and worked on my computer and a record is added for signup (JohnDoe) in users table.

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = "https://zeeimyqduvannrxhevws.supabase.co";
+const supabaseAnonKey = "sb_publishable_7G4Xyr48CXq-4Yq8n-udjw_spmISndL";
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
